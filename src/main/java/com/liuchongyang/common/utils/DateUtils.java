13a3131d6e48c393be8bbeb29494b191e80f5560
lcy <2428405519@qq.com>
2019-12-24 10:23:58 +0800

    n commit

--- src/main/java/com/liuchongyang/common/utils/DateUtils.java
+++ src/main/java/com/liuchongyang/common/utils/DateUtils.java
@@ -28,45 +28,45 @@ public class DateUtils {
 		}
 		return age;
 	}
-	
+
 	public static Date getMonthEnd(Date date) {
 		Calendar c = Calendar.getInstance();
-		//用传入的日期初始化日历类
+		// 用传入的日期初始化日历类
 		c.setTime(date);
-		//让当前月份+1
+		// 让当前月份+1
 		c.add(Calendar.MONTH, 1);
-		//让日期变成下个月月初
-		
+		// 让日期变成下个月月初
+
 		Date init = getMonthInit(c.getTime());
-		
-		//用月初再次初始日历类
+
+		// 用月初再次初始日历类
 		c.setTime(init);
-		//日期的秒数-1
+		// 日期的秒数-1
 		c.add(Calendar.SECOND, -1);
-		
+
 		return c.getTime();
 	}
-	
+
 	public static Date getMonthInit(Date date) {
-		//获取的当前系统的时间的日历类
+		// 获取的当前系统的时间的日历类
 		Calendar c = Calendar.getInstance();
-		//用传入的日期初始化日历类
+		// 用传入的日期初始化日历类
 		c.setTime(date);
-		//设置月份的天为 1
+		// 设置月份的天为 1
 		c.set(Calendar.DAY_OF_MONTH, 1);
 		c.set(Calendar.HOUR_OF_DAY, 0);
 		c.set(Calendar.MINUTE, 0);
 		c.set(Calendar.SECOND, 0);
-		
+
 		return c.getTime();
 	}
-	
+
 	public static Date random(Date minDate, Date maxDate) {
 		// 开始毫秒数
 		long t1 = minDate.getTime();
 		// 结束毫秒数
 		long t2 = maxDate.getTime();
-        //介于t2 t1之间的毫米数
+		// 介于t2 t1之间的毫米数
 		long t3 = (long) (Math.random() * (t2 - t1 + 1) + t1);
 
 		return new Date(t3);
